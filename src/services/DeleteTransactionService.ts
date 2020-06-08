8efc1dd6f4d231d4f3f45b84332faaa2c67fc62d
arielff3 <arielfrancoferreira4@gmail.com>
2020-06-08 20:10:30 -0300

    :alien: Update ARIEL Commit CODE# 080620202010

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,20 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsReporsitory = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionsReporsitory.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionsReporsitory.remove(transaction);
   }
 }
 
